f2a96552252b81dda07583940c88e1e08e60e3af
FarmerQi <1009604488@qq.com>
2017-12-02 17:02:59 +0800

    Version-1.0

---
+++ Farm/MyApplication/app/src/main/java/com/example/farmerqi/farm/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.farmerqi.farm;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView navImage_1;
+    private ImageView navImage_2;
+    private ImageView navImage_3;
+    private ImageView navImage_4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        navImage_1 = (ImageView)findViewById(R.id.first_button_image);
+//        navImage_2 = (ImageView)findViewById(R.id.second_button_image);
+//        navImage_3 = (ImageView)findViewById(R.id.third_button_image);
+//        navImage_4 = (ImageView)findViewById(R.id.fourth_button_image);
+//        navImage_1.setImageResource(R.drawable.pic);
+//        navImage_2.setImageResource(R.drawable.message);
+//        navImage_3.setImageResource(R.drawable.like);
+//        navImage_4.setImageResource(R.drawable.user);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Farm
+A Demo
+This is an App which I try to design and coding by myself.
+For now,it is not finished yet.
