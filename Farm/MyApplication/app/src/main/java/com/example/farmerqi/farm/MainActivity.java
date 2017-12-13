1a27e433d7fdc86f59fe8c7e46449b5897c34f3c
FarmerQi <1009604488@qq.com>
2017-12-13 17:00:27 +0800

    Add Navigation View

--- Farm/MyApplication/app/src/main/java/com/example/farmerqi/farm/MainActivity.java
+++ Farm/MyApplication/app/src/main/java/com/example/farmerqi/farm/MainActivity.java
@@ -1,31 +1,21 @@
 package com.example.farmerqi.farm;
 
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by FarmerQi on 2017/12/13.
+ */
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageView navImage_1;
-    private ImageView navImage_2;
-    private ImageView navImage_3;
-    private ImageView navImage_4;
-
+    Toolbar mToolbar;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        navImage_1 = (ImageView)findViewById(R.id.first_button_image);
-//        navImage_2 = (ImageView)findViewById(R.id.second_button_image);
-//        navImage_3 = (ImageView)findViewById(R.id.third_button_image);
-//        navImage_4 = (ImageView)findViewById(R.id.fourth_button_image);
-//        navImage_1.setImageResource(R.drawable.pic);
-//        navImage_2.setImageResource(R.drawable.message);
-//        navImage_3.setImageResource(R.drawable.like);
-//        navImage_4.setImageResource(R.drawable.user);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
     }
 }
