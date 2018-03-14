e8bef3fcb782e00225e78f57f437ad0170b479c3
FarmerQi <1009604488@qq.com>
2018-03-14 23:57:32 +0800

    add photo pic function

--- Farm/MyApplication/app/src/main/java/com/example/farmerqi/farm/HomeFragment.java
+++ Farm/MyApplication/app/src/main/java/com/example/farmerqi/farm/HomeFragment.java
@@ -20,7 +20,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_page_main,container,false);
+        View view = inflater.inflate(R.layout.fragment_first,container,false);
 
 
         return view;
